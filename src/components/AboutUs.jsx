b5c0185b089467e5c111cc60c8f7068f8aa0dc3a
Ebrahim Beiati-Asl <45979474+ebrahimbeiati@users.noreply.github.com>
2024-06-20 10:01:29 +0100

    some edit

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,8 +5,8 @@ const Card = ({ id, icon, title, description, imageUrl }) => (
   <>
     <input type="radio" name="slide" id={id} className="hidden" />
     <label htmlFor={id} className="card-label relative">
-      <div className="card-row flex items-start text-white">
-        <div className="card-icon bg-gray-800 rounded-full w-16 h-16 flex items-center justify-center m-2 p-4 ">
+      <div className="card-row flex flex-col items-center sm:flex-row text-white">
+        <div className="card-icon bg-gray-800 rounded-full w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center m-2 p-2 sm:p-4">
           {icon}
         </div>
         <div className="card-description flex flex-col justify-center opacity-0 transform translate-y-8 transition-opacity transition-transform duration-300 ease-in-out delay-150">
@@ -17,23 +17,41 @@ const Card = ({ id, icon, title, description, imageUrl }) => (
     </label>
     <style jsx>{`
       .card-label {
-        width: 7rem;
-        margin: 0 0.5rem;
+        width: 80%;
+        max-width: 7rem;
+        margin: 0.5rem;
         background-size: cover;
         cursor: pointer;
         overflow: hidden;
         border-radius: 1rem;
-        transition: width 1s ease-in-out;
+        transition: all 1s ease-in-out;
         box-shadow: 0px 10px 30px -5px rgba(0, 0, 0, 0.8);
         background-image: url(${imageUrl});
       }
       input:checked + label {
-        width: 37.5rem;
-        height: 27rem;
+        width: 100%;
+        max-width: 30rem;
+        height: auto;
       }
       input:checked + label .card-description {
         opacity: 1 !important;
         transform: translateY(0) !important;
+      }
+      @media (min-width: 640px) {
+        .card-label {
+          max-width: 9rem;
+        }
+        input:checked + label {
+          max-width: 24rem;
+        }
+      }
+      @media (min-width: 768px) {
+        .card-label {
+          max-width: 12rem;
+        }
+        input:checked + label {
+          max-width: 32rem;
+        }
       }
     `}</style>
   </>
@@ -55,7 +73,7 @@ const AboutUs = () => {
       icon: "2",
       title: "طراحی وب‌سایت",
       description:
-        " ایجاد وب‌سایت‌های واکنش‌گرا و کاربرپسند که به راحتی در تمامی دستگاه‌ها نمایش داده می‌شوند.",
+        "ایجاد وب‌سایت‌های واکنش‌گرا و کاربرپسند که به راحتی در تمامی دستگاه‌ها نمایش داده می‌شوند.",
       imageUrl:
         "https://images.unsplash.com/photo-1702933017536-44e01bff1111?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDU5OTUzMTR8&ixlib=rb-4.0.3&q=85",
     },
@@ -79,7 +97,10 @@ const AboutUs = () => {
   ];
 
   return (
-    <section className="text-white py-20 px-6 w-full flex flex-col items-center" id="about">
+    <section
+      className="text-white w-full flex flex-col items-center text-center p-4 sm:p-10 mt-12"
+      id="about"
+    >
       <div className="container mx-auto">
         <div className="text-center mb-12">
           <h2 className="text-4xl font-bold mb-4">About Us</h2>
@@ -90,8 +111,11 @@ const AboutUs = () => {
           </p>
         </div>
 
-        <div id="card-wrapper" className="w-full flex justify-center">
-          <div className="card-container flex">
+        <div
+          id="card-wrapper"
+          className="w-full flex justify-center items-center flex-wrap"
+        >
+          <div className="card-container flex flex-wrap justify-center">
             {cards.map((card) => (
               <Card key={card.id} {...card} />
             ))}
