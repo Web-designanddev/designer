58555763ab16aa1753990b71a39fb64ec2a4c431
Ebrahim Beiati-Asl <45979474+ebrahimbeiati@users.noreply.github.com>
2024-06-22 22:42:49 +0100

    add image to the about

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -3,13 +3,17 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const CardDeal = () => (
-  <section className="mr-20 ">
+  <section className="flex flex-col md:flex-row items-center md:items-start mr-20 p-6  shadow-lg rounded-lg overflow-hidden">
     <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>
+      <h2
+        className={`${styles.heading2} text-3xl md:text-4xl font-bold leading-tight`}
+      >
         <br className="sm:block hidden" /> با ما در نمایشگاه خلاقیت و نو آوری
-        همراه شوید !
+        همراه شوید!
       </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <p
+        className={`${styles.paragraph} max-w-[470px] mt-5 text-lg leading-relaxed`}
+      >
         ما با تمرکز بر طراحی وب سایت، بهینه‌سازی موتور جستجو، طراحی لوگو و کارت
         ویزیت، به شما کمک می‌کنیم تا برند خود را به شکوه و اعتباری بی‌نظیر
         برسانید. هدف ما ارائه راهکارهایی منحصر به فرد است که به شما کمک می‌کند
@@ -19,8 +23,12 @@ const CardDeal = () => (
       <Button styles={`mt-10`} />
     </div>
 
-    <div className={layout.sectionImg}>
-      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    <div className={`${layout.sectionImg} mt-10 md:mt-0`}>
+      <img
+        src={card}
+        alt="billing"
+        className="w-full md:w-[70%] h-auto object-cover transition-transform duration-300 transform hover:scale-105"
+      />
     </div>
   </section>
 );
